91667d464822ad994a57cab82fddd71b3b10d4e5
Fernando A <fernando.arconada@gmail.com>
2013-08-04 12:37:00 +0200

    listar apuntes como json

--- src/Fer/SifpeBundle/Controller/ApunteController.php
+++ src/Fer/SifpeBundle/Controller/ApunteController.php
@@ -26,4 +26,19 @@ abstract class ApunteController extends AbstractController {
         return array();
     }
 
+    /**
+     * Lista los objeto gestionados por el repositorio
+     * Se devuelve el listado y el total de objetos devueltos
+     *
+     * @param int $desdeMeses Numero de meses atras para los que mostrar el listado
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function listDesdeMesAction($desdeMeses = 0) {
+        $items = $this->entityRepository->findPorMes($desdeMeses);
+        $output['data'] = $items;
+        $output['totalPaginas'] = $this->entityRepository->getTotalMesesRegistrados();
+        $view = $this->view($output, 200);
+        return $this->handleView($view);
+    }
+
 }
