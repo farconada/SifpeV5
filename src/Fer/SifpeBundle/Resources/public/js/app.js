886559f81606717d475cb54413328053b4671dad
Fernando A <fernando.arconada@gmail.com>
2013-08-06 21:55:02 +0200

    borrar apunte en FrontEnd

--- src/Fer/SifpeBundle/Resources/public/js/app.js
+++ src/Fer/SifpeBundle/Resources/public/js/app.js
@@ -9,13 +9,28 @@ var sifpeApp = angular.module('sifpeApp', []).config(function($routeProvider, $i
     $interpolateProvider.startSymbol('[[').endSymbol(']]');
     $routeProvider.
         when("/list", {controller: 'GastoCtrl', templateUrl: "/bundles/fersifpe/templates/apunte_list.html"}).
+        when("/edit", {controller: 'GastoCtrl', templateUrl: "/bundles/fersifpe/templates/edit.html"}).
         otherwise({redirectTo: "/list"});
 });
 
 sifpeApp.controller('GastoCtrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http){
+    $scope.predicate = '-date';
+
     $http.get('gastos/1').success(function(data){
         $scope.apuntes = data['data'];
     });
+
+    $scope.edit = function(apunteIndex) {
+        var apunteAEditar = $scope.apuntes[apunteIndex];
+        alert(apunteAEditar.id);
+    };
+
+    $scope.delete = function(apunteIndex) {
+        var apunteABorrar = $scope.apuntes[apunteIndex];
+        $http.get('gasto/' + apunteABorrar.id + '/borrar').success(function(data){
+            $scope.apuntes.splice(apunteIndex, 1);
+        });
+    }
 
 }]);
 
