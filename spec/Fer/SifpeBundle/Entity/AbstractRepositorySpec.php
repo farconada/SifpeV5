6bad4e13f273963fde57bdea82b8a11513f62bba
Fernando A <fernando.arconada@gmail.com>
2013-10-06 11:26:54 +0200

    spec AbstractRepository

---
+++ spec/Fer/SifpeBundle/Entity/AbstractRepositorySpec.php
@@ -0,0 +1,46 @@
+<?php
+namespace spec\Fer\SifpeBundle\Entity;
+
+/**
+ * Created by JetBrains PhpStorm.
+ * User: fernando
+ * Date: 06/10/13
+ * Time: 11:15
+ * To change this template use File | Settings | File Templates.
+ */
+
+use PhpSpec\ObjectBehavior;
+use Fer\SifpeBundle\Entity\IEntidad;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\EntityManager;
+use Prophecy\Argument;
+
+class AbstractRepositorySpec extends ObjectBehavior {
+    public function let(
+        EntityManager $em,
+        ClassMetadata $classMetadata
+    ) {
+        $this->beConstructedWith($em, $classMetadata);
+    }
+
+    public function it_inherits_from_base_repository() {
+        $this->shouldHaveType('Doctrine\ORM\EntityRepository');
+    }
+    public function it_could_save_entities(
+        IEntidad $entity,
+        EntityManager $em
+    ) {
+        $this->save($entity);
+        $em->persist($entity)->shouldBeCalled();
+        $em->flush(Argument::cetera())->shouldBeCalled();
+    }
+
+    public function it_could_remove_entities(
+        IEntidad $entity,
+        EntityManager $em
+    ) {
+        $em->remove($entity)->shouldBeCalled();
+        $em->flush(Argument::cetera())->shouldBeCalled();
+        $this->remove($entity);
+    }
+}
