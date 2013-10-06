fb972cc558f6c05a91be9122fa308fcbefc1cdd7
Fernando A <fernando.arconada@gmail.com>
2013-10-06 11:29:47 +0200

    spec ApunteRepository

---
+++ spec/Fer/SifpeBundle/Entity/ApunteRepositorySpec.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: fernando
+ * Date: 06/10/13
+ * Time: 11:27
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace spec\Fer\SifpeBundle\Entity;
+
+use PhpSpec\ObjectBehavior;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\EntityManager;
+
+class ApunteRepositorySpec extends ObjectBehavior {
+    public function let(
+        EntityManager $em,
+        ClassMetadata $classMetadata
+    ) {
+        $this->beConstructedWith($em, $classMetadata);
+    }
+
+    public function it_inherits_from_my_base_repository() {
+        $this->shouldHaveType('Fer\SifpeBundle\Entity\AbstractRepository');
+    }
+}
