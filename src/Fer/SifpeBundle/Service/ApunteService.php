24baf238060c2281abfca0f11e179decbffc67ad
Fernando Arconada <fernando.arconada@gmail.com>
2013-10-18 10:48:52 +0200

    Servicios para manejar las entidades

--- src/Fer/SifpeBundle/Service/ApunteService.php
+++ src/Fer/SifpeBundle/Service/ApunteService.php
@@ -9,28 +9,12 @@
 
 namespace Fer\SifpeBundle\Service;
 
+use Fer\SifpeDomain\Model\IEntidad;
 use Fer\SifpeDomain\Repository\IApunteRepository;
 use FOS\ElasticaBundle\Finder\FinderInterface;
 
-class ApunteService {
-    /**
-     * @var IApunteRepository
-     */
-    private $repository;
+class ApunteService extends EntityService {
 
-    /**
-     * @var FinderInterface
-     */
-    private $finder;
-
-    /**
-     * @param IApunteRepository $repository
-     * @param FinderInterface $finder
-     */
-    public function __construct(IApunteRepository $repository, FinderInterface $finder) {
-        $this->repository = $repository;
-        $this->finder = $finder;
-    }
 
     /**
      * @param $queryString
@@ -38,7 +22,7 @@ class ApunteService {
      * @param \DateTime $dateEnd
      * @return array
      */
-    public function searchFullText($queryString, \DateTime $dateIni, \DateTime $dateEnd) {
+    public function searchFullText($queryString, \DateTime $dateIni, \DateTime $dateEnd, $limit = 500) {
         $queryString = new \Elastica\Query\QueryString($queryString);
         $queryObj = new \Elastica\Query($queryString);
         $queryFilter = new \Elastica\Filter\Range(
@@ -49,6 +33,59 @@ class ApunteService {
             )
         );
         $queryObj->setFilter($queryFilter);
-        return $this->finder->find($queryObj, 500);
+        return $this->finder->find($queryObj, $limit);
     }
+
+	/**
+	 * Devuelve los Apuntes de un mes ya sea el mes actual (por defecto) o X meses atras desde este mes
+	 * Se tienen en cuenta los meses enteros de mes a mes y no de dia a dia,
+	 * Si hoy es 14-06-2011 1 mes atras devolveria los apuntes de 1 al 31 de Mayo
+	 *
+	 * @param int $mesesAtras Numero de meses atras para los que devolver el listado
+	 * @return mixed
+	 */
+	public function findPorMes($mesesAtras = 0) {
+		$this->repository->findPorMes($mesesAtras);
+	}
+
+	/**
+	 * Devuelve un entero que representa el numero de meses que hay registrados en la base de datos desde hoy
+	 * Los meses corresponden a las paginas de las tablas
+	 * @return int
+	 */
+	public function getTotalMesesRegistrados() {
+		$this->repository->getTotalMesesRegistrados();
+	}
+
+	/**
+	 * Devuelve una lista de cuentas con el total por cuenta de cada mes
+	 *
+	 * @param $anio
+	 * @param $mes
+	 * @return array
+	 */
+	public function getTotalCuentasMensual($anio, $mes) {
+		$this->repository->getTotalMesesRegistrados($anio, $mes);
+	}
+
+	/**
+	 * Devuelve el total de los apuntes de un mes para los doces meses del año de enero a dicimbre
+	 *
+	 * @param $anio 2013, 2012, 2011....
+	 * @return array
+	 */
+	public function getResumenAnual($anio) {
+		$this->repository->getResumenAnual($anio);
+	}
+
+	/**
+	 * resumen de gastos e ingresos de un mes
+	 * @param $anio 2013, 2012, 2011.......
+	 * @param $mes 01 ó 1 02 03 04
+	 * @return array
+	 */
+	public function getResumenMes($anio, $mes) {
+		$this->repository->getResumenMes($anio, $mes);
+	}
+
 }
