08b7886c696d0b35dd024dc7df5fdebf4e4cb525
Fernando Arconada <fernando.arconada@gmail.com>
2013-10-21 11:21:07 +0200

    metodo guardar de CuentaRepo

--- src/Fer/SifpeDomain/Repository/ORM/CuentaRepository.php
+++ src/Fer/SifpeDomain/Repository/ORM/CuentaRepository.php
@@ -10,7 +10,21 @@
 namespace Fer\SifpeDomain\Repository\ORM;
 
 use Fer\SifpeDomain\Repository\ORM\AbstractRepository;
+use Fer\SifpeDomain\Model\IEntidad;
 
 class CuentaRepository extends AbstractRepository {
-
+	/**
+	 * Guarda una cuenta
+	 * Sobreescribe save() para reconstruir la relacion de objetos y que la tenga encuenta el
+	 * entity manager
+	 * @param IEntidad $apunte
+	 */
+	public function save(IEntidad $cuenta)
+	{
+		if($cuenta->getGrupo()) {
+			$grupo = $this->getEntityManager()->find('Fer\SifpeDomain\Model\GrupoCuenta', $cuenta->getGrupo()->getId());
+			$cuenta->setGrupo($grupo);
+		}
+		parent::save($cuenta);
+	}
 }
