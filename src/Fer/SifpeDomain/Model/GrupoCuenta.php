a7ded73b12c75a1720cd79e376367a75f2434388
Fernando Arconada <fernando.arconada@gmail.com>
2016-07-20 09:32:01 +0200

    campos de presupuesto anual y mensual para un grupo de cuenta

--- src/Fer/SifpeDomain/Model/GrupoCuenta.php
+++ src/Fer/SifpeDomain/Model/GrupoCuenta.php
@@ -28,31 +28,49 @@ class GrupoCuenta implements IEntidad
      * @ORM\Column(name="name", type="string", length=80)
      */
     private $name;
-	/**
-	 * @var \Doctrine\Common\Collections\ArrayCollection<Cuenta>
-	 * @ORM\OneToMany(mappedBy="grupo", targetEntity="Cuenta")
+    /**
+     * @var \Doctrine\Common\Collections\ArrayCollection<Cuenta>
+     * @ORM\OneToMany(mappedBy="grupo", targetEntity="Cuenta")
      * @JMS\Exclude
-	 */
-	private $cuentas;
+     */
+    private $cuentas;
 
-	/**
-	 * @return \Doctrine\Common\Collections\ArrayCollection
-	 */
-	public function getCuentas() {
-		return $this->cuentas;
-	}
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="presupuestoMes", type="decimal", precision=10, scale=2, options={"default": 0}))
+     * @JMS\Type("double")
+     */
+    protected $presupuestoMes;
 
-	/**
-	 * @param \Doctrine\Common\Collections\ArrayCollection $cuentas
-	 */
-	public function setCuentas($cuentas) {
-		$this->cuentas = $cuentas;
-	}
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="presupuestoAnual", type="decimal", precision=10, scale=2, options={"default": 0}))
+     * @JMS\Type("double")
+     */
+    protected $presupuestoAnual;
+
+    /**
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getCuentas()
+    {
+        return $this->cuentas;
+    }
+
+    /**
+     * @param \Doctrine\Common\Collections\ArrayCollection $cuentas
+     */
+    public function setCuentas($cuentas)
+    {
+        $this->cuentas = $cuentas;
+    }
 
     /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
@@ -81,4 +99,38 @@ class GrupoCuenta implements IEntidad
 
         return $this;
     }
+
+    /**
+     * @return float
+     */
+    public function getPresupuestoMes()
+    {
+        return $this->presupuestoMes;
+    }
+
+    /**
+     * @param float $presupuestoMes
+     */
+    public function setPresupuestoMes($presupuestoMes)
+    {
+        $this->presupuestoMes = $presupuestoMes;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPresupuestoAnual()
+    {
+        return $this->presupuestoAnual;
+    }
+
+    /**
+     * @param float $presupuestoAnual
+     */
+    public function setPresupuestoAnual($presupuestoAnual)
+    {
+        $this->presupuestoAnual = $presupuestoAnual;
+    }
+
+    
 }
