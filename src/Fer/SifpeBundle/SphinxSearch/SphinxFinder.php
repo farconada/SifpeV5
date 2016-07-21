19fc97b4571e3cd0afbca7b734b0c12a2bdecce3
F. Arconada <fernando.arconada@gmail.com>
2016-07-21 17:10:41 +0200

    busqueda por rango de fechas

--- src/Fer/SifpeBundle/SphinxSearch/SphinxFinder.php
+++ src/Fer/SifpeBundle/SphinxSearch/SphinxFinder.php
@@ -14,6 +14,9 @@ class SphinxFinder implements IFinderService {
 
    public function find($query) {
      $this->sphinx->setLimits(0, 9999);
+     $fromDate = \DateTime::createFromFormat('Y-m-d', $query['filter']['range']['fecha']['from']);
+     $toDate = \DateTime::createFromFormat('Y-m-d', $query['filter']['range']['fecha']['to']);
+     $this->sphinx->setFilterBetweenDates('unixdate', $fromDate, $toDate);
      $sphinxResults = $this->sphinx->searchEx($query['query'], $this->index);
      $results = [];
      foreach ($sphinxResults['matches'] as $id => $item) {
